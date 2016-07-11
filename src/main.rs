7095254fa725c3c368ebdca9c775b04467855e20
Tony Arcieri <bascule@gmail.com>
2016-07-10 19:22:24 -0700

    Initial main.rs
    Adds a stub "create" command. Not actually useful yet.
    
    Previous lib.rs renamed to server.rs.

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::path::Path;
+
+extern crate clap;
+use clap::{App, Arg, SubCommand};
+
+mod server;
+use server::LmdbAdapter;
+
+fn main() {
+    let version = "v0.1";
+
+    let create_command = SubCommand::with_name("create")
+                         .about("Creates a new ithos database")
+                         .arg(Arg::with_name("path")
+                              .help("Path where the database will be located")
+                              .index(1)
+                              .required(true));
+
+    let matches = App::new("ithos")
+                  .version(version)
+                  .subcommand(create_command)
+                  .get_matches();
+
+    if let Some(ref matches) = matches.subcommand_matches("create") {
+        let path = matches.value_of("path").unwrap();
+
+        println!("Creating database at: {}", path);
+        LmdbAdapter::create_database(Path::new(path)).unwrap();
+    }
+}
